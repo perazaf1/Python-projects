1ca6068a42e37d717cf357cc7542a75c4d8a6ffd
pe <80415605+perazaf1@users.noreply.github.com>
2023-03-08 09:50:48 +0000

    syntax correction

--- number_guessing.py
+++ number_guessing.py
@@ -3,15 +3,15 @@ import random
 
 # importer les chiffres à deviner
 number = random.randrange(1,20)
-guess = int(input("Entrez un nombre..."))
+guess = int(input("Entrez un nombre : "))
 
 while number != guess :
     if guess < number :
         print("Le nombre est trop bas !")
-        guess = int(input("Entrez un nombre..."))
+        guess = int(input("Entrez un nombre : "))
     elif guess > number : 
         print("Le nombre est trop haut !")
-        guess = int(input("Entrez de nouveau un nombre..."))
+        guess = int(input("Entrez de nouveau un nombre : "))
     else :
         break
 print("Vous l'avez deviné parfaitement !")
