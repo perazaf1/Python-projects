26e1bf2ab0a707e21660edb4b1b5b5922b4d6c06
pe <80415605+perazaf1@users.noreply.github.com>
2023-03-08 10:08:22 +0000

    added time module to project and comm modification

--- hangman.py
+++ hangman.py
@@ -2,7 +2,7 @@ import math
 import random
 import time
 
-# Eentrée du jeu
+# Début du jeu :
 print ('=== Bienvenue sur le jeu du pendu ===')
 name = input("Entrez votre nom : ")
 print(f'Bonjour {name}, je te souhaite une bonne chance ! ')
