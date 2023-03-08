0670861941f2780973c2af9d338a9f9c3da002a9
pe <80415605+perazaf1@users.noreply.github.com>
2023-03-08 09:49:29 +0000

    code programmm

--- number_guessing.py
+++ number_guessing.py
@@ -1,3 +1,21 @@
 import math
 import random
 
+# importer les chiffres à deviner
+number = random.randrange(1,20)
+guess = int(input("Entrez un nombre..."))
+
+while number != guess :
+    if guess < number :
+        print("Le nombre est trop bas !")
+        guess = int(input("Entrez un nombre..."))
+    elif guess > number : 
+        print("Le nombre est trop haut !")
+        guess = int(input("Entrez de nouveau un nombre..."))
+    else :
+        break
+print("Vous l'avez deviné parfaitement !")
+
+
+
+
